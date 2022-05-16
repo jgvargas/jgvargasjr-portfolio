dc7ffff2790e9487e595bc81fb20e1e1f8e48998
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2022-05-16 17:44:06 -0500

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      @jgvargasjr
+      <Navbar />
+      <div className="container">
+        <h2>Hello there</h2>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+export default function Navbar() {
+    return(
+        <nav>
+            <div className="container">
+                <div id="nav-logo">@Jgvargasjr</div>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
